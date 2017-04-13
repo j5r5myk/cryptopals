35b287e3082bfa755cc9ed70681b1e373a2932da
gptestudent <gptestudent@gptestudent.com>
2017-04-13 16:16:42 -0400

    hex2b64 function expects int[]

--- hex2b64/hex2b64.go
+++ hex2b64/hex2b64.go
@@ -39,47 +39,30 @@ func readHexLine() []byte {
   return rawhex
 }
 
-func isEvenLen(hexstr string) bool {
-  if (len(hexstr)-1)%2 != 0 {
-    return false
-  }
-  fmt.Println("hex length: ", len(hexstr)-1)
-  return true
-}
-
-func isHex(rawhex []byte) bool {
-  for i := 0; i < len(rawhex); i++ {
-    if (rawhex[i] < '0') || (rawhex[i] > 'f') {
-      fmt.Println(rawhex[i])
-      return false
-    }
-  }
-  return true
-}
 /*
 A lil map
 Bytes:  11111111 22222222 3333333
 b64s:   11111122 22223333 33444444 
 */
-func hex2b64(rawhex []byte) string {
+func hex2b64(ints []int) string {
   dict := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
   var b64 string
   var b int
   // process 3 bytes at a time
-  for i := 0; i < len(rawhex); i+=3 {
+  for i := 0; i < len(ints); i+=3 {
     // first 6 bits
-    b = (int) rawhex[i] & 0xFC >> 2
+    b = ints[i] & 0xFC >> 2
     b64 = b64 + dict[b]
     // next 6
-    b = (int) rawhex[i] & 0x03 << 4
-    b = b | (rawhex[i+1] & 0xF0 >> 4)
+    b = ints[i] & 0x03 << 4
+    b = b | (ints[i+1] & 0xF0 >> 4)
     b64 = b64 + dict[b]
     // 6 more
-    b = (int) rawhex[i+1] & 0x0F << 2
-    b = b | rawhex[i+2] & 0xC0 >> 4
+    b = ints[i+1] & 0x0F << 2
+    b = b | ints[i+2] & 0xC0 >> 4
     b64 = b64 + dict[b]
     // last 6
-    b = (int) rawhex[i+2] & 0x3F
+    b = ints[i+2] & 0x3F
     b64 = b64 + dict[b]
   }
   return b64
