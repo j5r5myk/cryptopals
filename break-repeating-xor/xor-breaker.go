14f217ff12e483d8d83bc05ec95fd941ae36846f
gptestudent <gptestudent@gptestudent.com>
2017-04-18 17:15:06 -0400

    End of day commit

--- break-repeating-xor/xor-breaker.go
+++ break-repeating-xor/xor-breaker.go
@@ -6,11 +6,28 @@ import (
 )
 
 func main() {
+  MIN_KEYSIZE := 2
+  MAX_KEYSIZE := 40
   str1 := "this is a test"
   str2 := "wokka wokka!!!"
- // read input
- // calc hamming d
- fmt.Println(calcHamming(str1, str2))
+  // read input
+  // Find key size
+  keySize := findKeySize(MIN_KEYSIZE, MAX_KEYSIZE)
+  // Divide input into keySized blocks
+  
+}
+func findKeySize(min int, max int input string) int {
+  lowHam := 1000
+  for i := min; i <= max; i++ {
+    // Hamming distance b/w first 2 chunks
+    rawHam := calcHamming(input[0:keysize], input[keysize, 2 * keysize])
+    // Normalize and compare
+    normHam := rawHam / keySize
+    if normHam < lowHam {
+      lowHam = normHam
+    }
+  }
+  return lowHam
 }
 func calcHamming(str1 string, str2 string) int {
   result := make([]byte, len(str1))
@@ -18,7 +35,7 @@ func calcHamming(str1 string, str2 string) int {
   // XOR each byte
   for i := 0; i < len(str1); i++ {
     result[i] = str1[i] ^ str2[i]
-    // Count differieng bytes
+    // Count differing bytes
     for result[i] > 0 {
       hamming++
       result[i] &= result[i] - 1
