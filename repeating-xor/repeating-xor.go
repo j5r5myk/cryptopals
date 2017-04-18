8e2a4593e376e3dabb53918ac712c29c36b21fb6
gptestudent <gptestudent@gptestudent.com>
2017-04-18 14:15:36 -0400

    Add string concat for multiline input

--- repeating-xor/repeating-xor.go
+++ repeating-xor/repeating-xor.go
@@ -3,15 +3,27 @@ package main
 import (
   "fmt"
   //"strconv"
+  "bufio"
+  "os"
   "encoding/hex"
+  "bytes"
 )
 
 func main() {
   key := "ICE"
-  input := "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+  //input := "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
   // Read input
+  scanner := bufio.NewScanner(os.Stdin)
+  // Concat all the lines
+  var buffer bytes.Buffer
+  for scanner.Scan() {
+    //fmt.Println(scanner.Text())
+    buffer.WriteString(scanner.Text())
+  }
+  // Test
+  fmt.Println(buffer.String())
   // Perform repeating xor
-  output := repeatXor(input, key)
+  output := repeatXor(buffer.String(), key)
   // Print result
   fmt.Printf("%s", hex.Dump(output))
 }
