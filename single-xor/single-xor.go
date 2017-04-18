9615af2acbcd7dc8fe8716dc89dbab48e0fb0ebc
gptestudent <gptestudent@gptestudent.com>
2017-04-18 11:49:17 -0400

    Fix types. Now it compiles. Add score keeping

--- single-xor/single-xor.go
+++ single-xor/single-xor.go
@@ -2,33 +2,41 @@ package main
 
 import (
   "fmt"
-  //"os"
+  "encoding/hex"
   "strconv"
 )
 
 func main() {
   input := "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
   letters := "etaoinsrhdlucmfywgpbvkxqjz"
+  // Arbitrary large value
+  lowscore := 100000
+  var score int
+  lowholder := make([]byte, len(input) / 2)
   // Create hashmap
-  freq := make(map[string]int)
+  freq := make(map[byte]int)
   for pos, char := range letters {
-    freq[char] = pos
+    freq[byte(char)] = pos
   }
   bytes := parseHexString(input)
-  for pos, char := range input {
+  for _, char := range bytes {
     decoded := decode(char, bytes)
-    score := score(decoded, freq)
-    fmt.Printf("Decoded: %v, Score: %d\n", decoded, score)
-    // compare to previous attempts greedily
+    score = calcScore(decoded, freq)
+    fmt.Printf("Score: %d\n%s", hex.Dump(decoded), score)
+    // Greedily compare to previous attempts
+    if score < lowscore {
+      lowscore = score
+      copy(decoded, lowholder)
+    }
   }
-  // output winner
+  fmt.Printf("Winner:\n%s", hex.Dump(lowholder))
 }
 func parseHexString(s string) []byte {
-  outpout := make([]byte, len(s) / 2)
+  output := make([]byte, len(s) / 2)
   j := 0
   for i := 0; i < len(s); i++ {
     hibits, err := strconv.ParseInt(string(s[i]), 16, 0)
-    lowbits, err := strconv.ParseInt(string(s[i+1), 16, 0)
+    lowbits, err := strconv.ParseInt(string(s[i+1]), 16, 0)
     if err != nil {
       fmt.Println(1, err)
     }
@@ -37,16 +45,16 @@ func parseHexString(s string) []byte {
   }
   return output
 }
-func decode(r rune, bytes []byte) []byte {
+func decode(b byte, bytes []byte) []byte {
   result := make([]byte, len(bytes))
   for pos, char := range bytes {
-     result[pos] = char ^ r
+     result[pos] = char ^ b
   }
   return result
 }
-func score(s []byte, freq map[string]int) int {
+func calcScore(s []byte, freq map[byte]int) int {
   score := 0
-  for char, pos := range s {
+  for _, char := range s {
     score += freq[char]
   }
   return score
