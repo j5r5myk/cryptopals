9e236e8ad0443cb89333acc82f2c1d7324ad004a
j5r5myk <j5r5myk@gmail.com>
2020-06-02 14:05:19 -0400

    Start ex 10

--- set2/10/10.go
+++ set2/10/10.go
@@ -5,22 +5,46 @@
 package main
 
 import (
-  "fmt"
-  "os"
+    "fmt"
+    "os"
+    "io/ioutil"
+    "log"
 )
 
 func main() {
-  // IV = /x00/x00/x00
-  // encrypt m with ecb
-  // xor those?
+    const size := 16
+    const IV := "\x00\x00\x00"
+    // divide m into 16 byte blocks
+    // c = ECB(IV xor block0)
+    // ECB(c xor block1) until entire message is encrypted
+    
+    // Open plaintext file
+    if len(os.Args) < 2 {
+        fmt.Printf("Usage: 10 <path-to-plaintext-file>\n")
+        os.Exit(0)
+    }
+    plaintext, err := ioutil.ReadFile(os.Args[1])
+    if err != nil {
+        log.Fatal(err)
+        os.Exit(0)
+    }
+    fmt.Printf("%s", plaintext)
+    // Init
+    ecb_encrypt(myXOR(IV, plaintext[0:16]))
+    // Loop through remaining blocks
+    for bs, be := 16, size+16; bs < len(plaintext); bs, be = bs+size, be+size {
+        
+    }
 }
+/*
 func ecb_encrypt(message, key []byte) []byte {
-  cipher, _ := aes.NewChiper([]byte(key))
-  ciphertext := make([]byte, len(message))
-  size := 16
-  // I copied this from Stackoverflow and don't understand it
-  for bs, be := 0, size; bs < len(data); bs, be = bs+size, be+size {
-    cipher.Encrypt(message[bs:be], ciphertext[bs:be])
-  }
-  return ciphertext
+    cipher, _ := aes.NewChiper([]byte(key))
+    ciphertext := make([]byte, len(message))
+    size := 16
+    // Encrypt a block at a time
+    for bs, be := 16, size+16; bs < len(data); bs, be = bs+size, be+size {
+      cipher.Encrypt(message[bs:be], ciphertext[bs:be])
+    }
+    return ciphertext
 }
+*/
