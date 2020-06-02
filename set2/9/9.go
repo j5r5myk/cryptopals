824db6fd64aa763c713339d2c23e21e17c361eb4
xjermsx <lostinthefilesystem@gmail.com>
2020-06-02 01:23:28 -0400

    Make ex 9 read white space

--- set2/9/9.go
+++ set2/9/9.go
@@ -7,32 +7,38 @@ package main
 import (
   "fmt"
   "os"
+  "bufio"
+  "strings"
 )
 
 func main() {
-  fmt.Println("Welcome to PKSC#7 padding\nPlease enter a block:")
-  var input string
-  fmt.Scanln(&input)
-  fmt.Println("Please enter padding size:")
-  var size int
-  fmt.Scanln(&size)
-  padded := pad([]byte(input), size)
-  fmt.Printf("Result: %s length: %d\n", string(padded), len(padded))
+    var size int
+    reader := bufio.NewReader(os.Stdin)
+
+    fmt.Println("Welcome to PKSC#7 padding\nPlease enter a block:")
+    // Read line from stdin 
+    input, _ := reader.ReadString('\n')
+    // Remove trailing newline
+    input = strings.TrimSuffix(input, "\n")
+    fmt.Println("Please enter padding size:")
+    fmt.Scanln(&size)
+    padded := pad([]byte(input), size)
+    fmt.Printf("Result: %s length: %d\n", string(padded), len(padded))
 }
 
 func pad(input []byte, size int) []byte {
-  if len(input) > size {
-    fmt.Println("Padding error: block is larger than length")
-    os.Exit(1)
-  } else if len(input) == size {
-    return input
-  } else {
-    tail := make([]byte, size - (len(input)))
-    for i, _ := range tail {
-      tail[i] = 0x04
+    if len(input) > size {
+      fmt.Println("Padding error: block is larger than desired padding length")
+      os.Exit(1)
+    } else if len(input) == size {
+      return input
+    } else {
+      tail := make([]byte, size - (len(input)))
+      for i, _ := range tail {
+        tail[i] = 0x04
+      }
+      out := append(input, tail...)
+      return out
     }
-    out := append(input, tail...)
-    return out
-  }
-  return []byte("bogus")
+    return []byte("bogus")
 }
